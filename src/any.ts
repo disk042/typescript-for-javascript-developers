4c3529232344b9cd257af2430a2a6161c40cdac0
DaisukeTashiro <62758430+disk042@users.noreply.github.com>
2021-03-11 23:10:33 +0900

    Update any.ts

--- src/any.ts
+++ src/any.ts
@@ -2,9 +2,16 @@ import axios from 'axios';
 
 export {};
 
-let url = 
-    'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
+let url =
+  'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
 axios.get(url).then(function (response) {
-    console.log(response.data);
+    interface Article {
+      id: number;
+      title: string;
+      description: string;
+    }
+    let data: Article[]; //object[]の中でも正確な制約をもった型とする。
+    data = response.data;
+    console.log(data);
 });
