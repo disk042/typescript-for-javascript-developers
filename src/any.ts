e11bd2739c835749f7ed6790c44fda40cb7e1f11
DaisukeTashiro <62758430+disk042@users.noreply.github.com>
2021-03-11 09:05:00 +0900

    Update any.ts

--- src/any.ts
+++ src/any.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export {};
 
-let url =
-  'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
+let url = 
+    'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
 axios.get(url).then(function (response) {
     console.log(response.data);
